030e006daf7c3d5271cf7ca4f584968ce112dfdf
Lars <lakrsv@gmail.com>
2018-08-11 11:13:37 +0100

    Added basic movement & cursor tracking

---
+++ Assets/Scripts/Transform/PlayerInput.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlayerInput.cs" author="Lars" company="None">
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerInput : MonoBehaviour
+{
+    private const float MaxVelocity = 3f;
+
+    private Rigidbody2D _rigidBody;
+
+    private float _acceleration = 50f;
+
+    private Vector2 _movement = new Vector2();
+
+    // Use this for initialization
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var inputH = Input.GetAxisRaw("Horizontal");
+        var inputV = Input.GetAxisRaw("Vertical");
+
+        _movement.Set(inputH, inputV);
+        _movement.Normalize();
+
+        _rigidBody.AddForce(_movement * _acceleration);
+        _rigidBody.velocity = Vector2.ClampMagnitude(_rigidBody.velocity, MaxVelocity);
+    }
+}
---
+++ Assets/Scripts/Transform/RotateWithCursor.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RotateWithCursor.cs" author="Lars" company="None">
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+public class RotateWithCursor : MonoBehaviour
+{
+    private Transform _transform;
+
+    private Camera _mainCamera;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+        _mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var mouseWorldPos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0;
+
+        _transform.rotation = Quaternion.LookRotation(Vector3.forward, mouseWorldPos - _transform.position);
+    }
+}
